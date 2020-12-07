2de96338eeb455c000fddf684654c88bb8eda0fb
Jason Herrera <baytownjason@yahoo.com>
2020-12-07 15:23:38 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,9 +1,2 @@
 # Write your solution here
-run_code_inside = false
-puts "Code before if...end"
-if run_code_inside
-  puts 'code inside'
-else
-  puts 'this is the else'
-end
-puts 'Code after if...end'
+Time.year.now
